f0334758c89b4d38399e3659b6b9af98d79dc086
alcb1310 <andres@andrescourt.com>
2024-03-01 18:53:17 -0500

    Brew formula update for kanban version v0.0.2

--- kanban.rb
+++ kanban.rb
@@ -5,12 +5,12 @@
 class Kanban < Formula
   desc "Kanban CLI - a cli tool for managin kanban boards"
   homepage "https://github.com/alcb1310/kanban"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
 
   on_macos do
-    url "https://github.com/alcb1310/kanban/releases/download/v0.0.1/kanban_0.0.1_darwin_all.tar.gz"
-    sha256 "ea3284c6be3b05eb56c099eba7d2074ee732a23d8b145a58d073f93e3b54527d"
+    url "https://github.com/alcb1310/kanban/releases/download/v0.0.2/kanban_0.0.2_darwin_all.tar.gz"
+    sha256 "65434ee222d9183e1ddd813a6291e0b5fa3dbfd71f0f1c26fe4ae21235e22389"
 
     def install
       bin.install "kanban"
@@ -19,16 +19,16 @@ class Kanban < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/alcb1310/kanban/releases/download/v0.0.1/kanban_0.0.1_linux_arm64.tar.gz"
-      sha256 "69ddfcd7ea69692bafa62936c16ba3a548b91242d76430662154508ecdf25738"
+      url "https://github.com/alcb1310/kanban/releases/download/v0.0.2/kanban_0.0.2_linux_arm64.tar.gz"
+      sha256 "a06a08e388d18f8b30442ea4d675466e1ec7ebb1e48987ac505a5c2c402c9b6e"
 
       def install
         bin.install "kanban"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/alcb1310/kanban/releases/download/v0.0.1/kanban_0.0.1_linux_amd64.tar.gz"
-      sha256 "3bf6187aa4a0369ac8a10ddfebde6a6bb77f2bab3c901ee4f69a31fa67ceca0a"
+      url "https://github.com/alcb1310/kanban/releases/download/v0.0.2/kanban_0.0.2_linux_amd64.tar.gz"
+      sha256 "a8bb009fac45219b0514c7ffc4a61f0f03c1618466c2c26c791a2718faa8cfe0"
 
       def install
         bin.install "kanban"
