3ef6f759fca0d8e0f8851bbfae6c47ad5f059ede
alcb1310 <andres@andrescourt.com>
2024-03-01 22:46:53 -0500

    Brew formula update for kanban version v0.0.12

--- kanban.rb
+++ kanban.rb
@@ -5,12 +5,12 @@
 class Kanban < Formula
   desc "Kanban CLI - a cli tool for managin kanban boards"
   homepage "https://github.com/alcb1310/kanban"
-  version "0.0.9"
+  version "0.0.12"
   license "MIT"
   depends_on :macos
 
-  url "https://github.com/alcb1310/kanban/releases/download/v0.0.9/kanban_0.0.9_darwin_all.tar.gz"
-  sha256 "ba1c6ecd0bbf2cca53029ee6fd6e8d3e0093d3076a1e0baf0011d703d908cfab"
+  url "https://github.com/alcb1310/kanban/releases/download/v0.0.12/kanban_0.0.12_darwin_all.tar.gz"
+  sha256 "e66ef31e87712939c70cb4a1aa83278288c2088ec4ab04607936bad01a2499e8"
 
   def install
     bin.install "kanban"
