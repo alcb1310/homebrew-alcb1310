25c056f46dea65cc6eb1edb86410951f050c27e1
Andres Court <andres@andrescourt.com>
2024-04-14 17:11:41 -0500

    update url information

--- cli-tool.rb
+++ cli-tool.rb
@@ -1,7 +1,7 @@
 class CliTool < Formula
   desc "A tutorial CLI tool"
-  homepage "<https://github.com/alcb1310/cli-tool>"
-  url "<https://github.com/alcb1310/cli-tool/releases/download/v0.0.1/0.0.1.tar.gz>"
+  homepage "https://github.com/alcb1310/cli-tool"
+  url "https://github.com/alcb1310/cli-tool/releases/download/v0.0.1/0.0.1.tar.gz"
   version "0.0.1"
   license "MIT"
   sha256 "d5c5948bb9509d7cfaad9fe7b12df70aae6925b13131e662ccf920526dceac1a"
